5b813c720129d3d1da6dc7cca5d8b560fd332161
tarkptel <tspatel462@gmail.com>
2026-06-25 10:17:41 +0530

    some edit releted vd

--- frontend.py
+++ frontend.py
@@ -2,8 +2,13 @@ import streamlit as st
 from app import start, create_vectorstore, ask_llm
 import tempfile
 
+from db import save_chat, load_chat
+
+
 pdf = st.file_uploader("Upload File..")
 
+
+session_id = "user_1"
 
 # For PDF path
 import tempfile
@@ -19,6 +24,7 @@ def save_pdf(uploaded_file):
 
         return tmp.name
     
+    
 if pdf and "vectorstore" not in st.session_state:
 
     pdf_path = save_pdf(pdf)
@@ -29,16 +35,23 @@ if pdf and "vectorstore" not in st.session_state:
     
     
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    st.session_state.messages = load_chat(session_id)
 
 for msg in st.session_state.messages:
-
     with st.chat_message(msg["role"]):
         st.write(msg["content"])
 
 question = st.chat_input("Ask a question")
 
+
 if question:
+
+    st.session_state.messages.append({
+        "role": "user",
+        "content": question
+    })
+
+    save_chat(session_id, "human", question)
 
     answer = ask_llm(
         question,
@@ -46,14 +59,11 @@ if question:
     )
 
     st.session_state.messages.append({
-        "role": "user",
-        "content": question
-    })
-
-    st.session_state.messages.append({
         "role": "assistant",
         "content": answer
     })
+
+    save_chat(session_id, "ai", answer)
 
     st.rerun()
     
